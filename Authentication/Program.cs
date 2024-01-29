17f1e24fc191a956e387ecafeab79dec481fc556
Yogendra Maarisetty <yogendra.maarisetty@wisetechglobal.com>
2024-01-29 15:06:36 +0530

    Authentication Tutorial

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -1,6 +1,77 @@
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.Services.AddDataProtection();
+// builder.Services.AddHttpContextAccessor();
+// builder.Services.AddScoped<AuthService>();
+
+
+builder.Services.AddAuthentication("cookie")
+	.AddCookie("cookie");
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.Use((ctx, next) =>
+// {
+// 	var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
+// 	var protector = idp.CreateProtector("auth-cookie");
+// 	var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
+// 	var protectedPayload = authCookie.Split("=").Last();
+// 	var payload = protector.Unprotect(protectedPayload);
+// 	var parts = payload.Split(":");
+// 	var key = parts[0];
+// 	var value = parts[1];
+//
+// 	var claims = new List<Claim>();
+// 	claims.Add(new Claim(key, value));
+// 	var identity = new ClaimsIdentity(claims);
+// 	ctx.User = new ClaimsPrincipal(identity);
+// 	return next();
+// });
+
+app.UseAuthentication();
+app.MapGet("/username", (HttpContext ctx) =>
+{
+	
+	return ctx.User.FindFirstValue("usr");
+});
+
+app.MapGet("/login", async (HttpContext ctx) =>
+{
+	//authService.SignIn();
+	var claims = new List<Claim>();
+	claims.Add(new Claim("usr", "yogi"));
+	var identity = new ClaimsIdentity(claims, "cookie");
+	var user = new ClaimsPrincipal(identity);
+	await ctx.SignInAsync("cookie", new ClaimsPrincipal());
+	
+	return "ok";
+});
+
+
 
 app.Run();
+
+
+// public class AuthService
+// {
+// 	private readonly IDataProtectionProvider _idp;
+// 	private readonly IHttpContextAccessor _accessor;
+//
+// 	public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
+// 	{
+// 		_idp = idp;
+// 		_accessor = accessor;
+// 	}
+//
+// 	public void SignIn()
+// 	{
+// 		var protector = _idp.CreateProtector("auth-cookie");
+// 		_accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:yogi")}";
+// 	}
+// }
